9478731bc550d4f559a6a05af986695f9727dc25
amitportnoy <amitportnoy@dalet.com>
2018-05-10 17:09:47 +0300

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyPageComponent } from './lobby-page/lobby-page.component';
 import { RoomPageComponent } from './room-page/room-page.component';
+import { MatButtonModule, MatCardModule, MatDividerModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { GameTypeCardComponent } from './game-type-card/game-type-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LobbyPageComponent,
-    RoomPageComponent
+    RoomPageComponent,
+    GameTypeCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    FlexLayoutModule,
+
+    MatDividerModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/game-type-card/game-type-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-type-card',
+  templateUrl: './game-type-card.component.html',
+  styleUrls: ['./game-type-card.component.scss']
+})
+export class GameTypeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
